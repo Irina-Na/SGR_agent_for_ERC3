d95617ab5da294a119cdb3b9417d0d0575fcebfe
Irina Nosal <76039301+Irina-Na@users.noreply.github.com>
2025-12-02 22:25:11 +0100

    erc3-dev Implement core functionality for vanilla NextStep SGR Agent for erc3-dev, including main session management and task execution Score 56.2

---
+++ sgr-vanilla-agent-erc3/main.py
@@ -0,0 +1,48 @@
+import textwrap
+from openai import OpenAI
+from erc3 import ERC3
+from dotenv import load_dotenv
+load_dotenv()
+
+from store_agent import run_agent
+
+client = OpenAI()
+core = ERC3()
+MODEL_ID = "gpt-4.1"
+
+# Start session with metadata
+res = core.start_session(
+    benchmark="erc3-dev",
+    workspace="my",
+    name=f"NextStep SGR Agent ({MODEL_ID}) from ERC3 Samples",
+    architecture="NextStep SGR Agent with OpenAI")
+
+status = core.session_status(res.session_id)
+print(f"Session has {len(status.tasks)} tasks")
+
+for task in status.tasks:
+    print("="*40)
+    print(f"Starting Task: {task.task_id} ({task.spec_id}): {task.task_text}")
+    # start the task
+    core.start_task(task)
+    try:
+        run_agent(MODEL_ID, core, task)
+    except Exception as e:
+        print(e)
+    result = core.complete_task(task)
+    if result.eval:
+        explain = textwrap.indent(result.eval.logs, "  ")
+        print(f"\nSCORE: {result.eval.score}\n{explain}\n")
+
+core.submit_session(res.session_id)
+
+
+
+
+
+
+
+
+
+
+
